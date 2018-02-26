e00021b84ba20682572f10420ec220932561363b
Randal Humphries <randal@randalhumphries.com>
2018-02-26 15:08:39 -0500

    Creates 'employee' model
    Why:
    
    * Create model for employees
    
    This change addresses the need by:
    
    * Creating the 'employee' model
    * Creating the 'employee' factory
    * Creating the 'employee' spec

---
+++ app/models/employee.rb
@@ -0,0 +1,22 @@
+class Employee < ApplicationRecord
+  validates :temp_hire_at, presence: true
+  validate  :hire_date_valid?
+
+  before_validation :create_temp_hire_date
+
+  belongs_to :person
+
+  private
+
+    def hire_date_valid?
+      if temp_hire_at.present? && temp_hire_at > Date.today
+        errors.add(:temp_hire_at, "can't be in the future")
+      end
+    end
+
+    def create_temp_hire_date
+      if full_time_hire_at.present? && temp_hire_at.present? == false
+        self.temp_hire_at = full_time_hire_at
+      end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226180638) do
+ActiveRecord::Schema.define(version: 20180226193028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employees", force: :cascade do |t|
+    t.boolean "active", default: true
+    t.date "temp_hire_at"
+    t.date "full_time_hire_at"
+    t.bigint "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_employees_on_person_id"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "first_name"
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 20180226180638) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "employees", "people"
 end
