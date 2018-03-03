8d49c05b731836ecc6accf554a9a173958d43077
Randal Humphries <randal@randalhumphries.com>
2018-03-03 10:53:01 -0500

    Revert "Closes #26: Creates 'api' controller"
    Why:
    * This reverts commit 67c728da481260c32c5121043555646c46af2fbe
    * Change the approach to API namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   use_doorkeeper
   devise_for :users
-
-  namespace :api do
-    get "/" => "api#index"
-  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'factory_bot'
-require 'devise'
-require 'support/devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -63,6 +61,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include RequestSpecHelper, type: :request
 end
