fc35a61ef0e764bcb444c577f4e0248f46bdcb57
Randal Humphries <randal@randalhumphries.com>
2018-03-03 15:12:35 -0500

    Closes #27: Creates 'v1' controller
    Why:
    
    * Provide base controller for the v1 API
    
    This change addresses the need by:
    
    * Creating the 'v1' controller
    * Adding the routes for the 'v1' controller
    * Creating the 'v1' spec
    * Creating the 'generic_response' and 'header' Jbuilder templates
    * Creating the 'request spec' helper to provide Devise helper methods
      in request specs
    
    Other changes:
    
    * Removing 'lockable' attribute from 'user' model
    * Configuring RSpec for example results persistences

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   namespace :api do
     get "/" => 'api#index'
+    get "/v1" => "v1#index"
+    get "/v1/not_found" => 'v1#test_not_found'
+    get "/v1/not_authorized" => 'v1#test_not_authorized'
+    get "/v1/application_error" => 'v1#test_application_error'
   end
 
   use_doorkeeper
--- spec/requests/api/api_spec.rb
+++ spec/requests/api/api_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::ApiController, type: :request do
 
+  before(:each) do
+    @headers = { 'ACCEPT': 'application/json',  'CONTENT-TYPE': 'application/json' }
+  end
+
   describe "Base API" do
 
     it "returns http success" do
-      get api_path, params: nil, headers: { "HTTP_ACCEPT" => 'application/json'}
+      get "/api", params: nil, headers: @headers
       expect(response).to have_http_status(:success)
       u = JSON.parse(response.body).deep_symbolize_keys
       expect(u[:message]).to eq("Hello!")
