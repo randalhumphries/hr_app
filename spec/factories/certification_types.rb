02b4dd0b267284993592af2adf2505434484cfe8
Randal Humphries <randal@randalhumphries.com>
2018-03-08 22:40:04 -0500

    Fixes bug in 'certification type' factory
    Why:
    
    * Address brittleness in the Certification Type factory used in specs
    
    This change addresses the need by:
    
    * Changing the 'name' trait of the 'certification_type' factory to a
      randomized string

--- spec/factories/certification_types.rb
+++ spec/factories/certification_types.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :certification_type do
-    name                    "BBPD"
+    name                    { Faker::Lorem.words(1) }
     effective_interval       1
     effective_interval_unit "year"
   end
